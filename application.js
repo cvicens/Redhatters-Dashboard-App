af34c246783fc0a0bdb9b44a76f563ec00429c2a
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-06-05 16:09:58 +0200

    Lots of changes

--- application.js
+++ application.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(corser.create());
 
 //app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/www'));
 
 app.options("*", function (req, res) {
   // CORS
